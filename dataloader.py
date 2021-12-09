055dc8dafd8d9ecf021c6290d0c3347a534b81a1
dragonboliero <wmilczarek@gmail.com>
2021-12-09 19:57:23 +0100

    Created function loading global hiscores from remote file
    First commit in online-hiscores branch.
    Function get_global_hiscores() gets global hiscores data from
    remote Google spreadsheet and returns it as a dictionary.

--- dataloader.py
+++ dataloader.py
@@ -1,26 +1,54 @@
 import csv
+import gspread
 from collections import defaultdict
 
-# Function importing data from CSV file
+# Function importing data from CSV file.
 def data_from_csv(countries_dict, list_of_countries):
     with open("data/countries_data.csv", "r") as file:
         csv_data = csv.reader(file)
         for row in csv_data:
-            # Assigning each row item to a specific category
+            # Assigning each row item to a specific category.
             country, capital, continent, code = row
-            # Creating Python dictionary with this data
+            # Creating Python dictionary with this data.
             countries_dict[country] = {
                 "capital": capital, "continent": continent, "code": code}
         list_of_countries = list(countries_dict.keys())
         return list_of_countries
 
-
+#Function for getting user hiscores.
 def get_scores():
     hiscores_columns = defaultdict(list)
     with open('data/hiscores.csv', 'r') as data:
         reader = csv.DictReader(data)
         for row in reader:
-            # Assigning values from each column to a list
+            # Assigning values from each column to a list.
             for (column, value) in row.items():
                 hiscores_columns[column].append(value)
     return hiscores_columns
+
+#Function for getting global hiscores from a remote file.
+def get_global_hiscores():
+    #This function requires a .json file generated in Google Cloud Platform..
+    #It connects the script with Google Account.
+    #Info how to get it: https://www.youtube.com/watch?v=bu5wXjz2KvU
+    service_account = gspread.service_account()
+    #Open file with data
+    data_base = service_account.open('CQHiscores')
+
+    #Assign spreadsheets to variables.
+    capitals_sheet = data_base.worksheet('capitals')
+    flags_sheet = data_base.worksheet('flags')
+    continents_sheet = data_base.worksheet('continents')
+
+    #Get player names and results from 10 best players in each mode.
+    capitals_global_scores = capitals_sheet.get('A1:B10')
+    flags_global_scores = flags_sheet.get('A1:B10')
+    continents_global_scores = continents_sheet.get('A1:B10')
+    
+    #Assingn collected data to a dictionary.
+    global_scores_dictionary = {}
+    global_scores_dictionary["global_capitals"] = capitals_global_scores
+    global_scores_dictionary["global_flags"] = flags_global_scores
+    global_scores_dictionary["global_continents"] = continents_global_scores
+    
+    return global_scores_dictionary
