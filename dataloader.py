597f166742d5488f63877631746d52f65fe62a01
dragonboliero <wmilczarek@gmail.com>
2021-12-10 21:29:03 +0100

    Partialy added support for get_global_hiscores() to the app.
    First table of hiscores menu now supports get_global_hiscores()
    function. The table was extended by one column (user name) and
    one row (app user score).
    
    Additionally the method has been modified so that it now searches
    for app user best scores in all modes.

--- dataloader.py
+++ dataloader.py
@@ -2,6 +2,8 @@ import csv
 import gspread
 from collections import defaultdict
 
+from pyasn1.type.univ import Null
+
 # Function importing data from CSV file.
 def data_from_csv(countries_dict, list_of_countries):
     with open("data/countries_data.csv", "r") as file:
@@ -27,7 +29,7 @@ def get_scores():
     return hiscores_columns
 
 #Function for getting global hiscores from a remote file.
-def get_global_hiscores():
+def get_global_hiscores(username):
     #This function requires a .json file generated in Google Cloud Platform..
     #It connects the script with Google Account.
     #Info how to get it: https://www.youtube.com/watch?v=bu5wXjz2KvU
@@ -44,11 +46,33 @@ def get_global_hiscores():
     capitals_global_scores = capitals_sheet.get('A1:B10')
     flags_global_scores = flags_sheet.get('A1:B10')
     continents_global_scores = continents_sheet.get('A1:B10')
+
+    #Get user's position from the spreadsheets
+    user_capitals = capitals_sheet.find(username)
+    user_flags = flags_sheet.find(username)
+    user_continents = continents_sheet.find(username)
     
     #Assingn collected data to a dictionary.
     global_scores_dictionary = {}
-    global_scores_dictionary["global_capitals"] = capitals_global_scores
-    global_scores_dictionary["global_flags"] = flags_global_scores
-    global_scores_dictionary["global_continents"] = continents_global_scores
-    
+    global_scores_dictionary["capitals"] = capitals_global_scores
+    global_scores_dictionary["flags"] = flags_global_scores
+    global_scores_dictionary["continents"] = continents_global_scores
+    global_scores_dictionary[username] = []
+    if user_capitals == None:
+        global_scores_dictionary[username].append({'capitals':'No score yet'})
+    else:
+        global_scores_dictionary[username].append({'capitals':capitals_sheet.cell(user_capitals.row,user_capitals.col+1).value})
+    if user_flags == None:
+        global_scores_dictionary[username].append({'flags':'No score yet'})
+    else:
+        global_scores_dictionary[username].append({'flags':flags_sheet.cell(user_flags.row,user_flags.col+1).value})
+    if user_continents == None:
+        global_scores_dictionary[username].append({'continents':'No score yet'})
+    else:
+        global_scores_dictionary[username].append({'continents':continents_sheet.cell(user_continents.row,user_continents.col+1).value})
+
+    print(global_scores_dictionary[username])
     return global_scores_dictionary
+
+
+get_global_hiscores('Andre')
